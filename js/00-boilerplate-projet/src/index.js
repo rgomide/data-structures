e0ec174cd33485b019bac0eb129078c0d6a9b9f2
rgomide <renato.s.gomide@gmail.com>
2023-02-03 15:48:54 -0300

    .

--- js/00-boilerplate-projet/src/index.js
+++ js/00-boilerplate-projet/src/index.js
@@ -1,7 +1,20 @@
 function hello() {
-    return 'world'
+  return 'world'
+}
+
+function sum(a, b) {
+  if (isNumber(a) && isNumber(b)) {
+    return a + b
+  } else {
+    return undefined
+  }
+}
+
+function isNumber(value) {
+  return typeof value == 'number'
 }
 
 module.exports = {
-    hello
+  hello,
+  sum
 }
--- js/00-boilerplate-projet/tests/index.test.js
+++ js/00-boilerplate-projet/tests/index.test.js
@@ -1,4 +1,4 @@
-const { hello } = require('../src/index')
+const { hello, sum } = require('../src/index')
 
 describe('tests for index.js', () => {
 
@@ -7,6 +7,25 @@ describe('tests for index.js', () => {
     test('it returns hello', () => {
       const helloResponse = hello()
       expect(helloResponse).toStrictEqual('world')
+    })
+
+  })
+
+  describe('function sum', () => {
+
+    test('it returns 2 + 3 = 5', () => {
+      const result = sum(2, 3)
+      expect(result).toStrictEqual(5)
+    })
+
+    test('it returns 2 - 4 = -2', () => {
+      const result = sum(2, -4)
+      expect(result).toStrictEqual(-2)
+    })
+
+    test('it returns 3 + "abc" = undefined', () => {
+      const result = sum(3, 'abc')
+      expect(result).toBeUndefined()
     })
 
   })
