96e0fc0e264c8d2fee16031741ba7b780d15e4e1
rgomide <renato.s.gomide@gmail.com>
2023-09-19 22:42:52 -0300

    .

--- java/binary-tree/src/main/java/br/com/gomide/binary_tree/BinaryTree.java
+++ java/binary-tree/src/main/java/br/com/gomide/binary_tree/BinaryTree.java
@@ -5,66 +5,67 @@ public class BinaryTree<T extends Comparable<?>> implements IBinaryTree<T> {
   @Override
   public Node<T> createTree(T element) {
     // TODO Auto-generated method stub
-    return null;
+    throw new UnsupportedOperationException("Unimplemented method 'createTree'");
   }
 
   @Override
   public Node<T> createTree(T[] elements) {
     // TODO Auto-generated method stub
-    return null;
+    throw new UnsupportedOperationException("Unimplemented method 'createTree'");
   }
 
   @Override
   public Integer degree(Node<T> rootNode, T nodeElement) {
-    return null;
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'degree'");
   }
 
   @Override
   public void insert(Node<T> rootNode, T element) {
     // TODO Auto-generated method stub
-
+    throw new UnsupportedOperationException("Unimplemented method 'insert'");
   }
 
   @Override
   public boolean remove(Node<T> rootNode, T nodeElement) {
     // TODO Auto-generated method stub
-    return false;
+    throw new UnsupportedOperationException("Unimplemented method 'remove'");
   }
 
   @Override
   public Node<T> getFather(Node<T> rootNode, T nodeElement) {
     // TODO Auto-generated method stub
-    return null;
+    throw new UnsupportedOperationException("Unimplemented method 'getFather'");
   }
 
   @Override
   public Node<T> getBrother(Node<T> rootNode, T nodeElement) {
     // TODO Auto-generated method stub
-    return null;
+    throw new UnsupportedOperationException("Unimplemented method 'getBrother'");
   }
 
   @Override
   public Node<T> getByElement(Node<T> rootNode, T element) {
     // TODO Auto-generated method stub
-    return null;
+    throw new UnsupportedOperationException("Unimplemented method 'getByElement'");
   }
 
   @Override
   public Integer calculateTreeDepth(Node<T> rootNode) {
     // TODO Auto-generated method stub
-    return null;
+    throw new UnsupportedOperationException("Unimplemented method 'calculateTreeDepth'");
   }
 
   @Override
   public Integer calculateNodeLevel(Node<T> rootNode, T nodeElement) {
     // TODO Auto-generated method stub
-    return null;
+    throw new UnsupportedOperationException("Unimplemented method 'calculateNodeLevel'");
   }
 
   @Override
   public String toString(Node<T> rootNode) {
     // TODO Auto-generated method stub
-    return null;
+    throw new UnsupportedOperationException("Unimplemented method 'toString'");
   }
 
 }
