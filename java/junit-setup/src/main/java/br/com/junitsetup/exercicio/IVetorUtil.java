409aae4e7f66d12598a43e0f329df5227cef700d
rgomide <renato.s.gomide@gmail.com>
2023-09-29 16:12:37 -0300

    .

--- java/junit-setup/src/main/java/br/com/junitsetup/exercicio/IVetorUtil.java
+++ java/junit-setup/src/main/java/br/com/junitsetup/exercicio/IVetorUtil.java
@@ -2,6 +2,40 @@ package br.com.junitsetup.exercicio;
 
 public interface IVetorUtil {
 
+  /**
+   * Copie o conteúdo do vetor a para o vetor b
+   * 
+   * @param a vetor de origem
+   * @param b vetor de destino
+   */
   public void copiarValores(int[] a, int[] b);
+
+  /**
+   * Copie o conteúdo do vetor a para o vetor b na ordem inversa
+   * 
+   * @param a vetor de origem
+   * @param b vetor de destino
+   */
+  public void copiarValoresInverso(int[] a, int[] b);
+
+  /**
+   * Soma o conteúdo de dois vetores e retorna o resultado em um novo vetor
+   * 
+   * @param a vetor <strong>A</strong>
+   * @param b vetor <strong>B</strong>
+   * @return vetor contendo a soma dos elementos em cada posição
+   */
+  public int[] somarVetores(int[] a, int[] b);
+
+  /**
+   * Intercala os elementos de dois vetores em um novo. Os vetores de entrada
+   * podem ser de tamanhos diferentes
+   * 
+   * @param a vetor <strong>A</strong>
+   * @param b vetor <strong>B</strong>
+   * @return novo vetor com os valores de <strong>A</strong> intercalados com o de
+   *         <strong>B</strong>
+   */
+  public int[] intercalarElementos(int[] a, int[] b);
 
 }
